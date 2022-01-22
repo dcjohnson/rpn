c658ad1c85422603ef3d782a5df795e69b0e80cf
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2022-01-22 11:54:28 -0700

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+import qualified Lib as Lib
+import System.IO
+import qualified Data.BigDecimal as BD
 
 main :: IO ()
-main = someFunc
+main = repl []    
+
+repl :: [BD.BigDecimal] -> IO ()
+repl stack = do
+  putStr "> "
+  hFlush stdout
+  l <- getLine
+  let nStack = stack ++ [(BD.fromString l)]
+    in do
+    Lib.printStack nStack
+    repl nStack 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
+    ( printStack
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.BigDecimal as BD
+
+printStack :: [BD.BigDecimal] -> IO ()
+printStack stack = printStackIndexed 1 stack
+
+printStackIndexed :: Int -> [BD.BigDecimal] -> IO ()
+printStackIndexed index (first:rest) = do
+  printStackIndexed (index + 1) rest
+  putStrLn ((show index) ++ ") " ++ (BD.toString first))
+printStackIndexed _ [] = return () 
